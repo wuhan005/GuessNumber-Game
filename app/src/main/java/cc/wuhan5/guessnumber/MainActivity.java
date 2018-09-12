09032a732634371f413a6339d0c93c32ca06ecfb
John Wu <524306184@qq.com>
2018-09-12 09:08:54 +0800

    修复输入不正确导致崩溃的bug

--- app/src/main/java/cc/wuhan5/guessnumber/MainActivity.java
+++ app/src/main/java/cc/wuhan5/guessnumber/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView ansText1, ansText2, ansText3, ansText4;
     private Button sumbitButton;
+    private Button againButton;
     private TextView logText;
     private TextView inputNum;
 
@@ -41,10 +43,26 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if(checkAnswer().equals("A4B0")){
                     showAnswer();
+                    againButton.setVisibility(View.VISIBLE); //开新游戏按钮出现
+
                     addLog("答对了！");
-                }else {
+                }else if(!checkAnswer().equals("")){
                     addLog(checkAnswer());
                 }
+            }
+        });
+
+        againButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                newGame();
+            }
+        });
+
+        ansText4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                beater();
             }
         });
 
@@ -68,6 +86,8 @@ public class MainActivity extends AppCompatActivity {
         inputNum = findViewById(R.id.inputNum);
 
         sumbitButton = findViewById(R.id.submitButton);
+        againButton = findViewById(R.id.againButton);
+        againButton.setVisibility(View.INVISIBLE);
 
         addLog("游戏开始！");
     }
@@ -95,25 +115,31 @@ public class MainActivity extends AppCompatActivity {
 
     private String checkAnswer(){
 
-        //A,B初始化
-        Integer A = 0;
-        Integer B = 0;
+        //数字输入有误，不为4位数
+        if(inputNum.length() < 4 || inputNum.length() > 4){
+            Toast.makeText(this, "输入不正确", Toast.LENGTH_SHORT).show();
+        }else {
+            //A,B初始化
+            Integer A = 0;
+            Integer B = 0;
 
-        for(Integer i = 0; i <= 3; i++){
-            //位置、值都是对的
-            if(String.valueOf(inputNum.getText().charAt(i)).toString().equals(numberList.get(i).toString())){
-                A++;
-            }else{
-                //位置不对，但包含值
-                if(inputNum.getText().toString().indexOf(answerArray[i].toString()) != -1){
-                    B++;
+            for (Integer i = 0; i <= 3; i++) {
+                //位置、值都是对的
+                if (String.valueOf(inputNum.getText().charAt(i)).toString().equals(numberList.get(i).toString())) {
+                    A++;
+                } else {
+                    //位置不对，但包含值
+                    if (inputNum.getText().toString().indexOf(answerArray[i].toString()) != -1) {
+                        B++;
+                    }
                 }
+
             }
 
+            //inputNum.setText("");
+            return "A" + A.toString() + "B" + B.toString();
         }
-
-        //inputNum.setText("");
-        return "A" + A.toString() + "B" + B.toString();
+            return "";
     }
 
     private void showAnswer(){
@@ -121,6 +147,25 @@ public class MainActivity extends AppCompatActivity {
         ansText2.setText(answerArray[1].toString());
         ansText3.setText(answerArray[2].toString());
         ansText4.setText(answerArray[3].toString());
+    }
+
+    private void newGame(){
+        logText.setText("");
+
+        ansText1.setText("?");
+        ansText2.setText("?");
+        ansText3.setText("?");
+        ansText4.setText("?");
+
+        addLog("新游戏");
+        againButton.setVisibility(View.INVISIBLE);
+        InitNumber();
+
+    }
+
+    private void beater(){
+        String Answer = numberList.get(0).toString() + numberList.get(1).toString()+ numberList.get(2).toString()+ numberList.get(3).toString();
+        Toast.makeText(this, Answer, Toast.LENGTH_SHORT).show();
     }
 
     //添加提示框信息
